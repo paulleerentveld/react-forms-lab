5c20f58b65db7d8e3c2c8f534d63f5bfaec2253d
paulleerentveld <paul@veldnet.com.au>
2022-05-10 19:37:01 +1000

    lab stuff

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,60 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]:  event.target.value,
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    if (this.state.username === "" || this.state.password === "")
+    return
+    else {
+    let formData = { username: this.state.username, password: this.state.password }
+    this.props.handleLogin(formData)
+  }
+};
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input 
+            id="username" 
+            name="username" 
+            type="text"
+            value={this.state.username}
+            onChange={event => this.handleChange(event)}
+             />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+            id="password" 
+            name="password" 
+            type="password"
+            value={this.state.password} 
+            onChange={event => this.handleChange(event)}
+            />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit"
+          
+          >Log in</button>
         </div>
       </form>
     );
@@ -31,3 +65,4 @@ class LoginForm extends React.Component {
 }
 
 export default LoginForm;
+
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,39 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      remainChars: 280,
+      message: ""
+    };
   }
+
+  remainingChars = event => {
+    console.log(event.value)
+    this.setState({
+      remainChars:  this.state.remainChars -1,
+      message: event.target.value
+    })
+  }
+
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+        type="text" 
+        name="message" 
+        id="message" 
+        maxLength={this.props.maxChars} 
+        onChange={event => this.remainingChars(event)} 
+        value={this.state.message}
+        />
+        <h1>{this.state.remainChars}</h1>
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+
